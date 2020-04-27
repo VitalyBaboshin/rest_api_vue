bfa927ee1b62528b0911f55520214d153b42feb8
VitalyBaboshin <vitaliy2@ro.ru>
2020-04-27 20:52:06 +0500

    Добавлена сервис учетной записи firebase

--- app.js
+++ app.js
@@ -1,34 +1,36 @@
 
-// import firebase from 'firebase/app'
-// import 'firebase/auth'
-// import 'firebase/database'
-import * as admin from 'firebase-admin';
 
+const firebase = require('firebase');
 const express  = require('express');
 const path = require('path');
 const {v4} = require('uuid');
 const app = express();
 
-// const admin = require('firebase-admin');
+const admin = require("firebase-admin");
+const serviceAccount = require("./rest-api-e90ff-firebase-adminsdk.json");
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://rest-api-e90ff.firebaseio.com"
+});
 
-// admin.initializeApp({
-//     credential: admin.credential.refreshToken(refreshToken),
-//     databaseURL: 'https://rest-api-e90f.firebaseio.com'
-// });
+firebase.initializeApp({
+    apiKey: "AIzaSyBLzWbOte_9Qc5ZBDlhhwe6mERXLpisKX4",
+    authDomain: "rest-api-e90ff.firebaseapp.com",
+    databaseURL: "https://rest-api-e90ff.firebaseio.com",
+    projectId: "rest-api-e90ff",
+    storageBucket: "rest-api-e90ff.appspot.com",
+    messagingSenderId: "163106349618",
+    appId: "1:163106349618:web:10daacb29460da6f5b522b"
+});
 
-// firebase.initializeApp({
-//     apiKey: "AIzaSyBLzWbOte_9Qc5ZBDlhhwe6mERXLpisKX4",
-//     authDomain: "rest-api-e90ff.firebaseapp.com",
-//     databaseURL: "https://rest-api-e90ff.firebaseio.com",
-//     projectId: "rest-api-e90ff",
-//     storageBucket: "rest-api-e90ff.appspot.com",
-//     messagingSenderId: "163106349618",
-//     appId: "1:163106349618:web:10daacb29460da6f5b522b"
-// });
+const db = firebase.database();
+const usersRef = db.ref("contacts");
 
-// firebase.auth().onAuthStateChanged(() => {
+// var authData = ref.getAuth();
 //
-// });
+// if (authData) {
+//     console.log("Authenticated user with uid:", authData.uid);
+// }
 
 let CONTACTS = [
     {id: v4(), name: 'Виталий', value: '+7-911-033-10-10', marked: false}
@@ -36,15 +38,25 @@ let CONTACTS = [
 
 app.use(express.json());
 
+// app.get('/api/contacts', (req, res) => {
+//     res.status(200).json(CONTACTS);
+// });
+
 app.get('/api/contacts', (req, res) => {
-    setTimeout(() => {
-        res.status(200).json(CONTACTS)
-    },1000)
+    usersRef.once("value", function(snapshot) {
+        //console.log(snapshot);
+        if (snapshot.val() == null) {
+            res.json({message: "Error: No user found", "result": false});
+        } else {
+            res.status(200).json({"message":"successfully fetch data", "result": true, "data": snapshot.val()});
+        }
+    });
 
 });
 
+
 app.post('/api/contacts', (req,res) => {
-    const contact = {...req.body, id: v4(), marked: false};
+    const contact = {...req.body, id: v4(), marked: true};
     CONTACTS.push(contact);
     res.status(201).json(contact)
 });
@@ -57,7 +69,7 @@ app.get('*', (req, res) => {
 
 // DELETE
 app.delete('/api/contacts/:id', (req, res) => {
-    CONTACTS = CONTACTS.filter(c => c.id !== req.params.id);
+    ref = CONTACTS.filter(c => c.id !== req.params.id);
     res.status(200).json({message: 'Контакт был удален'})
 });
 
